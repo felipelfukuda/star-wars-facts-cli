2f7818da9d74e46a50a1f4633281dc1f3e59aa17
felipelfukuda <felipelfukuda@gmail.com>
2020-03-12 11:16:06 -0400

    add notes for CLI class

--- lib/StarWarsFacts/cli.rb
+++ lib/StarWarsFacts/cli.rb
@@ -1,8 +1,31 @@
 class StarWarsFacts::CLI
 
+
+    #start should greet user and describe ap
+    #get data from API
+    #create new custom objects
+    #all inside start method
+
+    # deals with inputs (create a loop that keeps asking for info)
+    #EX. while input != "exit" do "whatever" check MUSIC CLI for Case/when examble
+    #gives a list of acceptable commands
+    # gets user input
+    #depending on what input, it reacts appropriately
+    #condition to check input for true value
+    #else say "choose correct input" or something
+
+    #exit command
+    #if input == "exit" 
+    #says "good bye!"
+    #kills program
+
+
+
+
     def start
-        puts "checking if this works"
-        binding.pry
+        puts "Hold while your facts are retrieved from a galaxy far, far away..."
+        puts "Welcome to StarWars Facts!"
+        
     end
 
 end

363c6cec1557b8b57ab38214d89d9930edd7e8dc
felipelfukuda <felipelfukuda@gmail.com>
2020-03-12 10:39:30 -0400

    first commit

---
+++ lib/StarWarsFacts.rb
@@ -0,0 +1,9 @@
+# environment file
+# loads all files we need to run our app
+
+require "StarWarsFacts/version"
+
+module StarWarsFacts
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/StarWarsFacts/version.rb
@@ -0,0 +1,3 @@
+module StarWarsFacts
+  VERSION = "1.0.0"
+end

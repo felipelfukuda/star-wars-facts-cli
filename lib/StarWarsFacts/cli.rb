9d1d0a015bee841809247013f835e976a5034d4c
felipelfukuda <felipelfukuda@gmail.com>
2020-03-13 09:54:59 -0400

    find new issues with APi connection

--- lib/StarWarsFacts/cli.rb
+++ lib/StarWarsFacts/cli.rb
@@ -1,73 +1,67 @@
 class StarWarsFacts::CLI
 
 
-    #start should greet user and describe ap
-    #get data from API
-    #create new custom objects
-    #all inside start method
-
-    # deals with inputs (create a loop that keeps asking for info)
-    #EX. while input != "exit" do "whatever" check MUSIC CLI for Case/when examble
-    #gives a list of acceptable commands
-    # gets user input
-    #depending on what input, it reacts appropriately
-    #condition to check input for true value
-    #else say "choose correct input" or something
-
-    #exit command
-    #if input == "exit" 
-    #says "good bye!"
-    #kills program
-
 
 
 
     def start
+        #how to get input to search for the proper person on SWAPI??
+        #case input (input = gets.strip.to_i)
+        #when 1 input = however we get to people/1 (luke)
+        #when 2 input = however we get to people/4 (han solo)
+        #etc.
         puts "----GALACTIC EMPIRE MOST WANTED FUGITIVES DATABASE----"
         puts "Please choose 1-9:"
-        puts "1. Luke Skywalker"
-        puts "2. C3PO"
-        puts "3. R2D2"
-        puts "4. "
-        puts "5. "
-        puts "6. "
-        puts "7. "
-        puts "8. "
-        puts "9. "
-        puts "10. "
+        puts "1. Luke Skywalker" #people/1
+        puts "2. C3PO" #people/2
+        puts "3. R2D2" #people/3
+        puts "4. Han Solo"  #people/14/
+        puts "5. Chewbacca" #people/13/
+        puts "6. Obi Wan Kenobi" #people/10/
+        puts "7. Lando Calrissian" #people/25/
+        puts "8. Leia Organa" #people/5/
+        puts "9. Master Yoda" #people/20/
+        puts "10. General Ackbar" #people/27/
         input = gets.strip.downcase
-        # case gets.strip.downcase
-        # when 'people'
-        #     choice == "1"
-        # when 'planets'
-        #     choice == "2"
-        # when 'starships'
-        #     choice == "3"
-        # end
-        # @data = StarWarsFacts::Api.get_data (api.rb method )
-        # @objects = StarWarsFacts::Info.all (info.rb class method)
         @data = StarWarsFacts::API.get_people(input)
         @objects = StarWarsFacts::Info.all
         display_info
     end
 
-  
+    #problem where the second input after "Y" will display information from the same choice as the first time
     def display_info
         puts "----------PROCESSING YOUR INQUIRY----------"
-        puts "-----------------------------------------------"
+        puts "--------------------------------------------"
         puts "Here are the specifications on ..... #{@data.name}"
-        @objects.each do |key|
-            puts "#{key.name}"
-        binding.pry
+        @objects.each do |subject|
+            puts "NAME :: #{subject.name.upcase}"
+            puts "BIRTH YEAR :: #{subject.birth_year.upcase}"
+            puts "GENDER :: #{subject.gender.upcase}"
+            puts "HEIGHT :: #{subject.height.upcase}"
+            puts "HAIR COLOR :: #{subject.hair_color.upcase}"
+            binding.pry
+        puts "WOULD YOU LIKE TO VIEW ANOTHER BOUNTY?"
+        puts "--------Y/N?------------"
+        input = gets.strip.downcase
+        if input == "y"
+            start
+        elsif input == "n"
+            puts "UPLOADING BOUNTY TO YOUR TRACKING FOB"
+            sleep 1
+            puts "UPLOADING..."
+            sleep 1
+            puts "UPLOADING..."
+            sleep 1
+            puts "UPLOAD COMPLETE!"
+            sleep 1
+            puts "GOODBYE AND HAPPY HUNTING"
+            sleep 1
+            exit
         end
-        #right here would go:
-        #@objects.each.with_index(1) {|obj| puts "#{index}. #{obj.name} - #{obj.age}"
-        #to display my list of objects to make a choice, depending on choices and attributes i represent
+        end
       
     end
 
-    def quit
-        puts "goodbye"
-    end
+
 
 end
